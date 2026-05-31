659cd67adab3b015c5f92eb551fdd1a91a230b7b
tako0614 <shoutatomiyama0614@gmail.com>
2026-05-31 20:36:05 +0000

    chore(bun): remove stale local-platform Deno entrypoints

--- src/worker/local-platform-entrypoints/local-platform/src/web.ts
+++
@@ -1,9 +0,0 @@
-import { startLocalWebServer } from "../../../local-platform/local-server.ts";
-import {
-  isDirectEntrypoint,
-  logEntrypointError,
-} from "../../../local-platform/direct-entrypoint.ts";
-
-if (await isDirectEntrypoint(import.meta.url)) {
-  startLocalWebServer().catch(logEntrypointError);
-}
